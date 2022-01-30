b1ba5288ce9987295fbb657f5e6345f5f4dd0caa
Michael Stewart <mikrostew@gmail.com>
2022-01-30 12:20:08 -0500

    convert to typescript, use TS word file

--- src/wordle-solver.ts
+++ src/wordle-solver.ts
@@ -1,15 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-
-// TODO: use typescript
-// TODO: also use eslint and prettier
+import { WORDS, WordAndFreq } from './words';
 
 class WordleSolver {
-  // using an array hasn't been too slow so far, but may need to do something else eventually
-  possibleWordsAndFreqs;
+  // using an array for this hasn't been too slow so far
+  private possibleWordsAndFreqs: WordAndFreq[];
 
   constructor() {
-    this.possibleWordsAndFreqs = loadWordAndFreqFile();
+    this.possibleWordsAndFreqs = WORDS;
   }
 
   // show all the words that are still possible
@@ -18,23 +14,31 @@ class WordleSolver {
   }
 
   // when a letter is part of the word, at a specific position
-  letterIncludedAtPosition(letter, position) {
-    this.possibleWordsAndFreqs = this.possibleWordsAndFreqs.filter((waf) => waf.word[position] === letter);
+  letterIncludedAtPosition(letter: string, position: number) {
+    this.possibleWordsAndFreqs = this.possibleWordsAndFreqs.filter(
+      (waf) => waf.word[position] === letter,
+    );
   }
 
   // when a letter is part of the word, but not at a specific position
-  letterIncludedNotAtPosition(letter, position) {
-    this.possibleWordsAndFreqs = this.possibleWordsAndFreqs.filter((waf) => waf.word.includes(letter) && waf.word[position] !== letter);
+  letterIncludedNotAtPosition(letter: string, position: number) {
+    this.possibleWordsAndFreqs = this.possibleWordsAndFreqs.filter(
+      (waf) => waf.word.includes(letter) && waf.word[position] !== letter,
+    );
   }
 
   // when a letter is not part of the word
-  letterNotIncluded(letter) {
-    this.possibleWordsAndFreqs = this.possibleWordsAndFreqs.filter((waf) => !waf.word.includes(letter));
+  letterNotIncluded(letter: string) {
+    this.possibleWordsAndFreqs = this.possibleWordsAndFreqs.filter(
+      (waf) => !waf.word.includes(letter),
+    );
   }
 
   // because I use this in a bunch of places
   howManyWordsLeft() {
-    console.log(`There are ${this.possibleWordsAndFreqs.length} words left now`);
+    console.log(
+      `There are ${this.possibleWordsAndFreqs.length} words left now`,
+    );
     return this.possibleWordsAndFreqs.length;
   }
 
@@ -42,7 +46,7 @@ class WordleSolver {
   // TODO: do this positionally
   getMostFrequentLetters() {
     // figure out the frequencies
-    const letterFrequencies = {};
+    const letterFrequencies: { [key: string]: number } = {};
     this.possibleWordsAndFreqs.forEach((waf) => {
       Array.from(waf.word).forEach((letter) => {
         if (letterFrequencies[letter] !== undefined) {
@@ -59,16 +63,20 @@ class WordleSolver {
       // sort descending
       return freqB - freqA;
     });
-    return sortedFreqs
+    return sortedFreqs;
   }
 
   // figure out which remaining word can be guessed using the input letter frequencies
-  getWordsWithLetters(letters, numWordsToFind) {
+  getWordsWithLetters(letters: string[], numWordsToFind: number) {
     // start by using the top 5 letters,
     // then increase number of letters until target num of words is found
-    const foundWords = {};
-    for (let useNumLetters = 5; useNumLetters <= letters.length; useNumLetters++) {
-      //console.log(`trying with ${useNumLetters} letters`);
+    const foundWords: { [key: string]: boolean } = {};
+    for (
+      let useNumLetters = 5;
+      useNumLetters <= letters.length;
+      useNumLetters++
+    ) {
+      // console.log(`trying with ${useNumLetters} letters`);
       const possibleWords = this.possibleWordsAndFreqs.filter((waf) => {
         const word = waf.word;
         // filter out words that use letters outside the top ones selected
@@ -79,9 +87,9 @@ class WordleSolver {
         }
         return true;
       });
-      //console.log(`found ${possibleWords.length} possible words`);
-      // TODO: sort words without duplicates to the top?
-      //console.log(possibleWords);
+      // console.log(`found ${possibleWords.length} possible words`);
+      // TODO: sort words without duplicate letters to the top
+      // console.log(possibleWords);
       // check if we found all the words yet
       for (let w = 0; w < possibleWords.length; w++) {
         // using more letters will cause duplicates with previous words, prevent that with a "set", of sorts
@@ -102,26 +110,13 @@ class WordleSolver {
     //       then guess a word containing all of those letters
     // TODO: does this already do hard mode?
     const mostFreqLetters = this.getMostFrequentLetters();
-    //console.log(`most frequent letters (high to low): ${mostFreqLetters.join(',')}`);
+    // console.log(`most frequent letters (high to low): ${mostFreqLetters.join(',')}`);
     // show 5 best guesses
     const possibleGuesses = this.getWordsWithLetters(mostFreqLetters, 20);
     console.log('some guesses:');
     console.log(possibleGuesses);
   }
 }
-
-// load the file containing words and frequencies
-function loadWordAndFreqFile() {
-  const allWordsAndFreqs = fs.readFileSync(path.resolve(__dirname, 'five-letter-words-and-frequencies.txt'), 'utf8').split('\n').map((line) => {
-    const wordAndFreq = line.split(',');
-    return {
-      word: wordAndFreq[0],
-      freq: wordAndFreq[1],
-    }
-  });
-  return allWordsAndFreqs;
-}
-
 
 // TODO: some kind of interactive CLI, because this way is not great
 
