9328a0a64f5836bb2e4ccc61238d967c9a23ec5d
Michael Stewart <mikrostew@gmail.com>
2022-02-02 23:14:03 -0500

    make the CLI nicer and easier to use

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,5 @@
 import WordleHelper from './wordle-helper';
+import chalk from 'chalk';
 import inquirer from 'inquirer';
 import yargs from 'yargs/yargs';
 
@@ -25,6 +26,8 @@ export async function run(rawArgs: string[]): Promise<void> {
 
   // let's solve this thing
   const wordleHelper = new WordleHelper();
+  // keep track of what has been found, to avoid repetitive prompts
+  const foundLetters = ['', '', '', '', ''];
 
   // limit of 6 guesses
   for (let guessNumber = 0; guessNumber < 6; guessNumber++) {
@@ -36,37 +39,52 @@ export async function run(rawArgs: string[]): Promise<void> {
       type: 'input',
       name: 'word',
       message: `What's your guess for word number ${guessNumber + 1}?`,
+      validate: (input) => {
+        if (/[a-zA-Z]{5}/.test(input)) {
+          return true;
+        }
+        return 'Please enter a 5-letter word';
+      },
     }]);
-    if (guess.word.length !== 5) {
-      // TODO: allow try again? should be a way to validate w/ inquirer...
-      throw new Error('5 letter words only');
-    }
     for (let i = 0; i < guess.word.length; i++) {
       const letter = guess.word[i];
+      if (letter === foundLetters[i]) {
+        // spaced to line up nicely under the other letters
+        console.log(`  (already found letter)            ${chalk.black.bgGreen(letter.toUpperCase())}`);
+        continue;
+      }
       const letterStatus = await inquirer.prompt([{
         type: 'list',
         name: 'status',
         message: `What is the status of letter '${letter.toUpperCase()}'?`,
         choices: [
-          '(gray)   Not in the word',
-          '(yellow) Right letter, wrong spot',
-          '(green)  Right letter, right spot',
+          `${chalk.black.bgGray(letter.toUpperCase())} (gray)   Not in the word`,
+          `${chalk.black.bgYellow(letter.toUpperCase())} (yellow) Right letter, wrong spot`,
+          `${chalk.black.bgGreen(letter.toUpperCase())} (green)  Right letter, right spot`,
         ],
       }]);
-      switch (letterStatus.status) {
-      case '(gray)   Not in the word':
+      const statusText = letterStatus.status;
+      switch (true) {
+      case /gray/.test(statusText):
         wordleHelper.letterNotIncluded(letter);
         break;
-      case '(yellow) Right letter, wrong spot':
+      case /yellow/.test(statusText):
         wordleHelper.letterIncludedNotAtPosition(letter, i);
         break;
-      case '(green)  Right letter, right spot':
+      case /green/.test(statusText):
         wordleHelper.letterIncludedAtPosition(letter, i);
+        foundLetters[i] = letter;
         break;
       default:
         console.log(letterStatus.status);
         throw new Error('bad choice here?');
       }
     }
+    // did we find all the letters?
+    if (foundLetters.every((l) => l !== '')) {
+      console.log(`The answer is ${chalk.black.bgGreen(guess.word.toUpperCase())}!`);
+      process.exit();
+    }
   }
+  console.log('Didn\'t find the answer :(');
 }
