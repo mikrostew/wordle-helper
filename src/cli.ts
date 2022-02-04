e79c8991d9b6f1c37f910cb045b974d8828373a6
Michael Stewart <mikrostew@gmail.com>
2022-02-04 15:37:40 -0500

    nicer cli output

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,12 @@ import chalk from 'chalk';
 import inquirer from 'inquirer';
 import yargs from 'yargs/yargs';
 
+interface FormattedLetter {
+  green: string;
+  yellow: string;
+  gray: string;
+}
+
 export async function run(rawArgs: string[]): Promise<void> {
   const parsedArgs = await yargs()
     .usage(
@@ -29,6 +35,7 @@ export async function run(rawArgs: string[]): Promise<void> {
   const wordleHelper = new WordleHelper(verbose);
   // keep track of what has been found, to avoid repetitive prompts
   const foundLetters = ['', '', '', '', ''];
+  const guessWords = ['', '', '', '', '', ''];
 
   // limit of 6 guesses
   for (let guessNumber = 0; guessNumber < 6; guessNumber++) {
@@ -40,6 +47,7 @@ export async function run(rawArgs: string[]): Promise<void> {
       type: 'input',
       name: 'word',
       message: `What's your guess for word number ${guessNumber + 1}?`,
+      transformer: (input) => input.toUpperCase(),
       validate: (input) => {
         if (/[a-zA-Z]{5}/.test(input)) {
           return true;
@@ -47,11 +55,17 @@ export async function run(rawArgs: string[]): Promise<void> {
         return 'Please enter a 5-letter word';
       },
     }]);
+    if (verbose) {
+      console.log('guess:', guess);
+    }
+
     for (let i = 0; i < guess.word.length; i++) {
       const letter = guess.word[i];
+      const formatted = formatLetter(letter);
       if (letter === foundLetters[i]) {
         // spaced to line up nicely under the other letters
-        console.log(`  (already found letter)            ${chalk.black.bgGreen(letter.toUpperCase())}`);
+        console.log(`  (already found letter)            ${formatted.green}`);
+        guessWords[guessNumber] += ` ${formatted.green}`;
         continue;
       }
       const letterStatus = await inquirer.prompt([{
@@ -59,22 +73,25 @@ export async function run(rawArgs: string[]): Promise<void> {
         name: 'status',
         message: `What is the status of letter '${letter.toUpperCase()}'?`,
         choices: [
-          `${chalk.black.bgGray(letter.toUpperCase())} (gray)   Not in the word`,
-          `${chalk.black.bgYellow(letter.toUpperCase())} (yellow) Right letter, wrong spot`,
-          `${chalk.black.bgGreen(letter.toUpperCase())} (green)  Right letter, right spot`,
+          `${formatted.gray} (gray)   Not in the word`,
+          `${formatted.yellow} (yellow) Right letter, wrong spot`,
+          `${formatted.green} (green)  Right letter, right spot`,
         ],
       }]);
       const statusText = letterStatus.status;
       switch (true) {
       case /gray/.test(statusText):
         wordleHelper.letterNotIncluded(letter);
+        guessWords[guessNumber] += ` ${formatted.gray}`;
         break;
       case /yellow/.test(statusText):
         wordleHelper.letterIncludedNotAtPosition(letter, i);
+        guessWords[guessNumber] += ` ${formatted.yellow}`;
         break;
       case /green/.test(statusText):
         wordleHelper.letterIncludedAtPosition(letter, i);
         foundLetters[i] = letter;
+        guessWords[guessNumber] += ` ${formatted.green}`;
         break;
       default:
         console.log(letterStatus.status);
@@ -83,9 +100,21 @@ export async function run(rawArgs: string[]): Promise<void> {
     }
     // did we find all the letters?
     if (foundLetters.every((l) => l !== '')) {
-      console.log(`The answer is ${chalk.black.bgGreen(guess.word.toUpperCase())}!`);
+      console.log('done!');
+      console.log('');
+      // make a nice printout of all the guesses (similar to Wordle)
+      console.log(guessWords.filter((line) => line !== '').join('\n\n'));
       process.exit();
     }
   }
   console.log('Didn\'t find the answer :(');
+}
+
+// letter formatting
+function formatLetter(letter: string): FormattedLetter {
+  return {
+    green: chalk.black.bgGreen(` ${letter.toUpperCase()} `),
+    yellow: chalk.black.bgYellow(` ${letter.toUpperCase()} `),
+    gray: chalk.black.bgGray(` ${letter.toUpperCase()} `),
+  };
 }
